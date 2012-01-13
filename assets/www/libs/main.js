ccb6701a62a597201ba33ccbc88590d65c9121f2
Ali Mihandoost <a.mihandoost@1dws.com>
2012-01-13 12:24:34 +0330

    Start des, Animate, Fix bug, ...

--- assets/www/libs/main.js
+++ assets/www/libs/main.js
@@ -1,5 +1,5 @@
 /*
- * <1Devs dot="com" department="Mobile" alt="www.1mobs.com" />
+ * <1Devs dot="com" department="Mobile" />
  *
  * 1Mobs.com - 1Devs Mobile Department.
  *
@@ -8,14 +8,11 @@
  */
 
 _(function(){
-	_('h1').tap(function(){
-		_.alert('Powered By _1js',null,'Hi');
-	});
-	_('h2').tap(function(){
-		_.log('Powered By _1js');
-	});
-	_('body>*').tap(function(){
-		_(this).css({color:'red'});
-	});
+	
+	// Go go go ! -mage paintbale
+	
+	$('.beforload').removeClass('beforload');
+	
+	// TODO: btns events ...
 	
 });
